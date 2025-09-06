e8b72a6868ec038d39e9d803d53b6c1b12a59923
saantiagg <santimunoz68@gmail.com>
2025-09-06 12:43:07 -0500

    Se crea la clase main, y se agregan los libros disponibles

--- Main.java
+++ Main.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[]args) { 
         Scanner sc=new Scanner(system.in);
-        Libro libro1=new Libro()
+        Libro libro1=new Libro("La divina comedia","Dante Alighieri",5,0);
+        Libro libro2=new Libro("Cien años de soledad","Gabriel Garcia Marquez",3,0);
+        Libro libro3=new Libro("El principito","Antoine de Saint-Exupery",4,0);
+
+        System.out.println("Catalogo de libros:");
+        System.out.println(libro1);
+        System.out.println(libro2);
+        System.out.println(libro3);
+
+        
+    
     }
 }
