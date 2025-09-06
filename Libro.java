2faad866c2c11765c3f4bb612f6153fa7d02a9f1
Yosman Farid <yosmanmu@gmail.com>
2025-09-06 15:42:24 -0500

    Cambios libro

--- Libro.java
+++ Libro.java
@@ -2,65 +2,58 @@ public class Libro {
     private String titulo;
     private String autor;
     private int cantidadDisponible;
-    private int cantidadPrestados; 
-
+    private int cantidadPrestados;
+ 
     public Libro(String titulo, String autor, int cantidadDisponible, int cantidadPrestados) {
         this.titulo = titulo;
         this.autor = autor;
         this.cantidadDisponible = cantidadDisponible;
         this.cantidadPrestados = cantidadPrestados;
-
+ 
     }
-
-    
-    
-
-        public String getTitulo() { return titulo;} 
+ 
+   
+   
+ 
+        public String getTitulo() { return titulo;}
         public void setTitulo(String titulo) { this.titulo = titulo; }
-
+ 
         public String getAutor() { return autor; }
         public void setAutor(String autor) { this.autor = autor; }
-
+ 
         public int getCantidadDisponible() { return cantidadDisponible; }
         public void setCantidadDisponible (int cantidadDisponible) { this.cantidadDisponible = cantidadDisponible; }
-
+ 
         public int getCantidadPrestados() { return cantidadPrestados; }
-        
+       
         public void setCatidadPrestados(int cantidadPrestados) { this.cantidadPrestados = cantidadPrestados; }
-
+ 
         public boolean prestar() {
             if (cantidadDisponible > 0) {
                 this.cantidadDisponible--;
                 this.cantidadPrestados++;
                 return true;
-            } 
+            }
             return false;
         }
-
-            
+ 
+           
          public boolean devolver() {
                 if (cantidadPrestados > 0) {
                     this.cantidadPrestados--;
                     this.cantidadDisponible++;
                     return true;
-                } 
+                }
                     return false;
                 }
-
-                @Override 
-
+ 
+                @Override
+ 
                 public String toString() {
-                    return "Libro{" +
-                            "titulo='" + titulo + '\'' +
-                            ", autor='" + autor + '\'' +
-                            ", cantidadDisponible=" + cantidadDisponible +
-                            ", cantidadPrestados=" + cantidadPrestados +
-                            '}';
+                    return "Libro: \n" +
+           "  Titulo: '"  + titulo + "',\n" +
+           "  autor:'" + autor + "',\n" +
+           "  Disponibles: " + cantidadDisponible + ",\n" +
+           "  Prestados: " + cantidadPrestados;
                 }
         }
-
-
-
-
-    
-
